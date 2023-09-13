9bc0a9f25ae489c960ff97fdd8f33df842698115
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-09-12 22:07:14 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Resume from './components/Resume';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <About />
+      <Work />
+      <Resume />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>
+            <a href="#work">My Work</a>
+          </li>
+          <li>
+            <h1><a href="#top">Sherilyn Koshy</a></h1>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+        </ul>
+      </nav>
+
+
+    </div>
+  )
+}
